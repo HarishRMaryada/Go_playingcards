03aacb40241613a99b18f3264d1bb4ece6b072dc
Harish Reddy Maryada <harishreddymaryada@Harishs-MacBook-Pro.local>
2020-04-04 18:43:59 +0530

    Read from file and convert to deck type

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
-func main() {
-	cards := newDeck()
-	// hand, remainingCards := deal(cards, 5)
-	// hand.print()
-	// remainingCards.print()
-	//fmt.Println(cards.toString())
-	cards.saveToFile("my_cards")
+import "fmt"
 
+func main() {
+	cards := newDeckFromFile("my_cards")
+	fmt.Println(cards)
 }
