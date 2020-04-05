5b86524976e0311e59e6d774bc9710e203687cd4
Harish Reddy Maryada <harishreddymaryada@Harishs-MacBook-Pro.local>
2020-04-05 16:55:45 +0530

    "Added testcase "

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,9 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 12 {
 		t.Errorf("Expected deck length of 12, but we got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spaces but got %v", d[0])
+	}
+
 }
