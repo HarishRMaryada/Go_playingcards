099be90d1a6e80613408f00360423582e590b192
Harish Reddy Maryada <harishreddymaryada@Harishs-MacBook-Pro.local>
2020-04-05 18:51:26 +0530

    itteration on maps

--- maps/main.go
+++ maps/main.go
@@ -3,16 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	colors := map[string]string{
+		"red":   "#123456",
+		"green": "#111111",
+		"White": "#fffff",
+	}
 
-	// var colors map[string]string
-	colors := make(map[string]string) //above eqvuvalent
-
-	// colors := map[string]string{
-	// 	"red":   "#123456",
-	// 	"green": "#111111",
-	// }
-
-	colors["White"] = "#fffff"
-	delete(colors, "White")
-	fmt.Print(colors)
+	for key, value := range colors {
+		fmt.Println(key, value)
+	}
 }
